7345d6951b48770f30ea410d4bb6bd321e833287
Matt Glazar <matt.glazar@gmail.com>
2013-06-10 21:26:14 -0700

    Kill -XTypeFamilies

--- B/RuleDatabase.hs
+++ B/RuleDatabase.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module B.RuleDatabase
