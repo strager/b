30df0bb33965f4e7968456da385ae2b1585c6d91
Matt Glazar <matt.glazar@gmail.com>
2013-06-10 20:59:11 -0700

    Clean up

--- B/RuleDatabase.hs
+++ B/RuleDatabase.hs
@@ -28,43 +28,43 @@ import Data.DynSet (DynSet)
 import qualified Data.DynSet as DynSet
 
 -- | A map from a question type 'q' to a value of type
--- 'RuleSets q'.
+-- 'RuleSet q'.
 newtype RuleDatabase
   = RuleDatabase (Map.Map TypeRep Any)
   deriving (Monoid, Typeable)
 
 -- A set of rule sets keyed by type.  The question type of
 -- all rule sets must be the same.
-newtype RuleSets q = RuleSets (DynSet (Rule q))
+newtype RuleSet q = RuleSet (DynSet (Rule q))
   deriving (Typeable)
 
-instance (Question q) => Rule q (RuleSets q) where
-  executeRule (RuleSets dynMap) q
+instance (Question q) => Rule q (RuleSet q) where
+  executeRule (RuleSet dynMap) q
     = asum $ DynSet.mapTo (`executeRule` q) dynMap
 
 instance (Question q) => Rule q RuleDatabase where
   executeRule rules q = lookupRS rules >>= (`executeRule` q)
 
 insert
-  :: forall q r. (Typeable r, Question q, Rule q r)
+  :: (Typeable r, Question q, Rule q r)
   => r -> RuleDatabase -> RuleDatabase
 insert x xs = singleton x <> xs
 
 lookupRS
   :: forall q. (Question q)
-  => RuleDatabase -> Maybe (RuleSets q)
+  => RuleDatabase -> Maybe (RuleSet q)
 lookupRS (RuleDatabase xs)
   = unsafeCoerce $ Map.lookup key xs
   where key = typeOf (undefined :: q)
 
 singletonRS
   :: forall q. (Question q)
-  => RuleSets q -> RuleDatabase
+  => RuleSet q -> RuleDatabase
 singletonRS x = RuleDatabase
   $ Map.singleton key (unsafeCoerce x)
   where key = typeOf (undefined :: q)
 
 singleton
-  :: forall q r. (Typeable r, Rule q r)
+  :: (Typeable r, Rule q r)
   => r -> RuleDatabase
-singleton x = singletonRS (RuleSets (DynSet.singleton x))
+singleton x = singletonRS (RuleSet (DynSet.singleton x))
