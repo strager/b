1896912a673f0cde65aeff36833876d91b9c1bf3
Matt Glazar <matt.glazar@gmail.com>
2013-06-11 22:33:56 -0700

    ST in-memory Oracle

--- B/Oracle/InMemory.hs
+++ B/Oracle/InMemory.hs
@@ -2,4 +2,5 @@ module B.Oracle.InMemory
   ( module X
   ) where
 
+import B.Oracle.InMemory.ST as X
 import B.Oracle.InMemory.STM as X
---
+++ B/Oracle/InMemory/ST.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+
+module B.Oracle.InMemory.ST
+  ( mkSTOracle
+  ) where
+
+import Control.Applicative
+import Control.Monad.ST.Safe
+import Data.STRef
+
+import B.Oracle (Oracle)
+
+import qualified B.Oracle as Oracle
+import qualified B.Oracle.InMemory.Pure as Pure
+
+mkSTOracle :: ST s (Oracle (ST s))
+mkSTOracle = mkSTOracleWithStorage
+  <$> newSTRef Pure.empty
+
+mkSTOracleWithStorage
+  :: STRef s (Pure.State (ST s))
+  -> Oracle (ST s)
+mkSTOracleWithStorage storage = Oracle.Oracle
+  { Oracle.get = \ q
+    -> Pure.get q <$> readSTRef storage
+  , Oracle.put = \ q a
+    -> modifySTRef storage (Pure.put q a)
+  , Oracle.dirty = \ q
+    -> modifySTRef storage (Pure.dirty q)
+  , Oracle.addDependency = \ from to
+    -> modifySTRef storage (Pure.addDependency from to)
+  }
