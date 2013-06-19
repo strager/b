9465f9f20fd7dc21afc0f2aa4845a552829cdb09
Matt Glazar <matt.glazar@gmail.com>
2013-06-18 19:59:15 -0700

    More unit tests

--- test/Spec/B/Build.hs
+++ test/Spec/B/Build.hs
@@ -59,11 +59,14 @@ oracle = PureOracle.mkOracle (M get) (M . modify)
 data R q = R (Map q [AQuestion M])
   deriving (Typeable)
 
-r :: (Ord q) => q -> [AQuestion M] -> R q
-r q deps = R $ Map.singleton q deps
+rdb
+  :: (Ord q, Question q, M ~ AnswerMonad q)
+  => q -> [AQuestion M] -> RuleDatabase M
+rdb q deps = RuleDatabase.singleton
+  . R $ Map.singleton q deps
 
 instance (Ord q) => Semigroup (R q) where
-  R a <> R b = R $ Map.unionWith mappend a b
+  R x <> R y = R $ Map.unionWith mappend x y
 
 instance (Ord q, Question q, M ~ AnswerMonad q)
   => Rule q (R q) where
@@ -76,46 +79,96 @@ needAQuestion_ :: AQuestion m -> BuildRule m ()
 needAQuestion_ (AQuestion q) = need_ q
 
 testBuild
-  :: RuleDatabase M
+  :: [RuleDatabase M]
   -> [LogMessage]
   -> Build M a
   -> Expectation
-testBuild db expectedLogs m = do
+testBuild dbs expectedLogs m = do
   logMessages <- execWriterT
     $ evalStateT go PureOracle.empty 
   logMessages `shouldBe` expectedLogs
   where
   logger message = M . lift $ tell [message]
   go = void . unM $ runBuild db oracle logger m
+  db = mconcat dbs
+
+a :: String -> AQuestion M
+a = AQuestion . A
+
+b :: String -> AQuestion M
+b = AQuestion . B
 
 spec :: Spec
 spec = do
   it "No rule" $ testBuild
-    RuleDatabase.empty
+    []
     [NoRuleError (A "hi")]
     $ build (A "hi")
 
   it "No rule for dependency" $ testBuild
-    (RuleDatabase.singleton $ r (A "hi") [AQuestion $ A "bye"])
-    [Building (A "hi"), NoRuleError (A "bye"), DoneBuilding (A "hi")]
-    $ build (A "hi")
+    [rdb (A "hi") [AQuestion $ A "bye"]]
+    [ Building (A "hi")
+    , NoRuleError (A "bye")
+    , DoneBuilding (A "hi")
+    ] $ build (A "hi")
 
   it "Build one" $ testBuild
-    (RuleDatabase.singleton $ r (A "hi") [])
+    [rdb (A "hi") []]
     [Building (A "hi"), DoneBuilding (A "hi")]
     $ build (A "hi")
 
   it "Build first rule" $ testBuild
-    (RuleDatabase.fromList [r (A "1") [], r (A "2") [], r (A "3") []])
+    [ rdb (A "1") []
+    , rdb (A "2") []
+    , rdb (A "3") []
+    ]
     [Building (A "1"), DoneBuilding (A "1")]
     $ build (A "1")
 
   it "Build second rule" $ testBuild
-    (RuleDatabase.fromList [r (A "1") [], r (A "2") [], r (A "3") []])
+    [ rdb (A "1") []
+    , rdb (A "2") []
+    , rdb (A "3") []
+    ]
     [Building (A "2"), DoneBuilding (A "2")]
     $ build (A "2")
 
   it "Build third rule" $ testBuild
-    (RuleDatabase.fromList [r (A "1") [], r (A "2") [], r (A "3") []])
+    [ rdb (A "1") []
+    , rdb (A "2") []
+    , rdb (A "3") []
+    ]
     [Building (A "3"), DoneBuilding (A "3")]
     $ build (A "3")
+
+  it "Chain with same rule type" $ testBuild
+    [ rdb (A "1") [a "2"]
+    , rdb (A "2") [a "3"]
+    , rdb (A "3") [a "4"]
+    , rdb (A "4") []
+    ]
+    [ Building (A "1")
+    , Building (A "2")
+    , Building (A "3")
+    , Building (A "4")
+    , DoneBuilding (A "4")
+    , DoneBuilding (A "3")
+    , DoneBuilding (A "2")
+    , DoneBuilding (A "1")
+    ] $ build (A "1")
+
+  it "Chain with interleaved rule type" $ testBuild
+    [ rdb (A "1") [b "2"]
+    , rdb (B "2") [a "3"]
+    , rdb (A "3") [b "4"]
+    , rdb (B "4") []
+    ]
+    [ Building (A "1")
+    , Building (B "2")
+    , Building (A "3")
+    , Building (B "4")
+    , DoneBuilding (B "4")
+    , DoneBuilding (A "3")
+    , DoneBuilding (B "2")
+    , DoneBuilding (A "1")
+    ] $ build (A "1")
